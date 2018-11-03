a9a5e931a520ff55b056180f92122dec5bac3568
tr1p <evelijnsaaltink@gmail.com>
2018-11-03 19:07:02 +0100

    Add a test script to pass tests

---
+++ src/js/script.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Powerlifting Tools', function() {
+	it('should return -1 when the value is not present', function() {
+		expect(2).to.equal(2);
+	})
+});
